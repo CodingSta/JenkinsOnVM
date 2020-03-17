c0515136cdef77449fed0fbd88f13f6f17315364
MinGyu, Ju <knighp@gmail.com>
2020-03-17 18:19:13 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+const app = express();
+var server = http.createServer(app);
+
+app.get('/', function(req, res){
+    res.send('root page!');
+});
+
+app.get('/start', function(req, res){
+    res.send('start page!');
+});
+
+server.listen(80, '127.0.0.1', function(){
+    console.log('Server listen on port!'+ server.address().port);
+});
