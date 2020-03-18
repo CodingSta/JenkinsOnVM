090ec8bd549a6c7d3b86c2a833408ac1ebe1a410
MinGyu, Ju <knighp@gmail.com>
2020-03-19 00:48:36 +0900

    good job

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('mingyu page!');
+    res.send('mingyu Good Job!');
 });
 
 app.get('/start', function(req, res){
