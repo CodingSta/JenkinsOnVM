5b5102e8e911bb0f3181f07ecddbb0e8a0a47de6
MinGyu, Ju <knighp@gmail.com>
2020-06-15 18:31:14 +0900

    sh
    sh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love JuSeHun!');
+    res.send('I love SeHun!');
 });
 
 app.get('/start', function(req, res){
