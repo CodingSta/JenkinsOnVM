33812abf62a5e73b8ad55089250de76deda9bbaf
MinGyu, Ju <knighp@gmail.com>
2020-06-15 13:07:48 +0900

    mingyu
    mingyu

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love you MinGyu3!');
+    res.send('I love you MinGyu!');
 });
 
 app.get('/start', function(req, res){
