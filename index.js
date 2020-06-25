0aac6023fe97def5363aca324b3e7b8d18218d16
MinGyu, Ju <knighp@gmail.com>
2020-06-25 16:23:26 +0900

    2nd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Test07!');
+    res.send('I like Test01!');
 });
 
 app.get('/start', function(req, res){
