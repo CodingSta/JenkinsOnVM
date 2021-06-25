2cc1a8c8de91dd4c256cd4b3dc29fc775cee70e4
jumingyu <knighp@gmail.com>
2021-06-25 13:35:34 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Test 3!');
+    res.send('I like IU!');
 });
 
 app.get('/start', function(req, res){
