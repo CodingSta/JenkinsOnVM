8636f9fb8fadfed628bb7ab796db194a172a1824
MinGyu, Ju <knighp@gmail.com>
2021-07-15 13:02:56 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like JuMinGyu!');
+    res.send('I like BTS!');
 });
 
 app.get('/start', function(req, res){
