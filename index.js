acc11c2e6fa92883080249bfac74a31281ece4e5
MinGyu, Ju <knighp@gmail.com>
2021-07-15 16:00:07 +0900

    12th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function (req, res) {
-    res.send('I like IUIUIU!');
+    res.send('I like IUIU!');
 });
 
 app.get('/start', function (req, res) {
