27bb71ef844eb31dff0fbf8e09251a99616b007f
MinGyu, Ju <knighp@gmail.com>
2021-07-15 15:52:09 +0900

    10th

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var http = require('http');
 const app = express();
 var server = http.createServer(app);
 
-app.get('/', function(req, res){
-    res.send('I like IU!');
+app.get('/', function (req, res) {
+    res.send('I like IUIU!');
 });
 
-app.get('/start', function(req, res){
+app.get('/start', function (req, res) {
     res.send('start page!');
 });
 
-server.listen(3000, '127.0.0.1', function(){
-    console.log('Server listen on port!'+ server.address().port);
+server.listen(3000, '127.0.0.1', function () {
+    console.log('Server listen on port!' + server.address().port);
 });
